17808ab7a726862d39fde7dd0c8b96e8600f1835
Ding Zhen <69374148+localhosts-A@users.noreply.github.com>
2024-08-06 16:45:59 +0800

    Update CI.sh

--- CI.sh
+++ CI.sh
@@ -4,7 +4,7 @@ O=out \
 ARCH=arm64 \
 CLANG_TRIPLE=aarch64-linux-gnu- \
 CROSS_COMPILE=/home/runner/Clang/bin/aarch64-linux-gnu- \
-CC=/home/runner/bin/clang \
-CROSS_COMPILE_ARM32=/home/runner/bin/arm-linux-gnueabi- "
+CC=/home/runner/Clang/bin/clang \
+CROSS_COMPILE_ARM32=/home/runner/Clang/bin/arm-linux-gnueabi- "
 make ${args} lmi_defconfig
 make ${args}

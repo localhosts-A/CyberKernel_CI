ed5fcc63c7eec1c054d50ce98eafe40aaacd0dc2
Ding Zhen <69374148+localhosts-A@users.noreply.github.com>
2024-08-06 16:21:11 +0800

    Update CI.sh

--- CI.sh
+++ CI.sh
@@ -3,8 +3,8 @@ args="-j$(nproc --all) \
 O=out \
 ARCH=arm64 \
 CLANG_TRIPLE=aarch64-linux-gnu- \
-CROSS_COMPILE=~/Clang/bin/aarch64-linux-gnu- \
-CC=~/Clang/bin/clang \
-CROSS_COMPILE_ARM32=~/Clang/bin/arm-linux-gnueabi- "
+CROSS_COMPILE=/home/runner/Clang/bin/aarch64-linux-gnu- \
+CC=/home/runner/bin/clang \
+CROSS_COMPILE_ARM32=/home/runner/bin/arm-linux-gnueabi- "
 make ${args} lmi_defconfig
 make ${args}
